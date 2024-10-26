64fe408760809e077696f44b3f4cf24502c6d902
vibol-1 <v1b01111lol@gmail.com>
2024-10-26 09:27:17 +0700

    Add app.py

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# ping-pong-game
-Ping pong
+
---
+++ spriteClass.py
@@ -0,0 +1,27 @@
+from pygame import *
+
+class GameSprite(sprite.Sprite):
+    def __init__(self, player_image, player_x, player_y, player_speed, width, height):
+        super().__init__()
+        self.image = transform.scale(image.load(player_image)(width, height)) 
+        self.speed = player_speed
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+
+    def reset(self, window_object):
+        window_object.blit(self.image, (self.rect.x, self.rect.y))
+
+class Player(GameSprite):
+    def update_right(self):
+        keys = key.get_pressed()
+        if keys[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y < 500 - 80:
+            self.rect.y += self.speed
+    def update_left(self):
+        keys = key.get_pressed()
+        if keys[K_w] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_s] and self.rect.y < 500 - 80:
+            self.rect.y += self.speed
